d3df77012947844edf6f42ea078f62759aa6c396
Evan <e.godon@outlook.com>
2021-01-18 17:42:59 -0500

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -1,39 +1,37 @@
-import re
 import os
 import subprocess
+import json
 from influxdb import InfluxDBClient
 
-HOST_TAG = os.environ['HOST_TAG']
-HOST = os.environ['INFLUXDB_HOST']
+HOST_TAG = os.environ["HOST_TAG"]
+HOST = os.environ["INFLUXDB_HOST"]
 PORT = 8086
-USER = os.environ['INFLUXDB_USER']
-PASSWORD = os.environ['INFLUXDB_PASSWORD']
-DB = 'internetspeed'
+USER = os.environ["INFLUXDB_USER"]
+PASSWORD = os.environ["INFLUXDB_PASSWORD"]
+DB = "internetspeed"
 
 client = InfluxDBClient(HOST, PORT, USER, PASSWORD, DB)
 
-response = subprocess.Popen('/usr/local/bin/speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
-ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
-download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
-upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
+response = (
+    subprocess.Popen(
+        "/usr/local/bin/speedtest-cli --json", shell=True, stdout=subprocess.PIPE
+    )
+    .stdout.read()
+    .decode("utf-8")
+)
 
-ping = ping[0].replace(',', '.')
-download = download[0].replace(',', '.')
-upload = upload[0].replace(',', '.')
+speedcli_json = json.loads(response)
 
 speed_data = [
     {
-        "measurement" : "internet_speed",
-        "tags" : {
-            "host": HOST_TAG
+        "measurement": "internet_speed",
+        "tags": {"host": HOST_TAG},
+        "fields": {
+            "download": float("{:.2f}".format(speedcli_json["download"] / 1000000)),
+            "upload": float("{:.2f}".format(speedcli_json["upload"] / 1000000)),
+            "ping": float("{:.2f}".format(speedcli_json["ping"])),
         },
-        "fields" : {
-            "download": float(download),
-            "upload": float(upload),
-            "ping": float(ping)
-        }
     }
 ]
 
 client.write_points(speed_data)
-
